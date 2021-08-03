84ccb50a66cea1047156dd81ffbe8150da410eca
GuilhermeRC <ecce.guilherme@gmail.com>
2021-08-03 11:53:05 -0300

    Inicio

---
+++ wk.dpr
@@ -0,0 +1,14 @@
+program wk;
+
+uses
+  Vcl.Forms,
+  uMain in 'view\uMain.pas' {FrmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmMain, FrmMain);
+  Application.Run;
+end.
